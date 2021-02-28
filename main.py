99c466ec63b7d87cf73dc6472aef06ed383d8bc0
Mohit Rathod <57721715+mohitrathod7@users.noreply.github.com>
2021-02-28 21:57:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
+from sqlalchemy import exc
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from urllib.parse import *
+from assistant import *
+from flask_socketio import SocketIO, send
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'Thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/database/database.db'
+
+socketio = SocketIO(app)
+
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -23,6 +31,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    logged_in = db.Column(db.String, nullable=False)
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -41,48 +51,91 @@ class RegisterForm(FlaskForm):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    session['next'] = request.args.get('next')
     form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
         if user:
             if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-        return '<h1>Invalid username or password</h1>'
-    return render_template('login.html', form=form)
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return '<h1>New user has been created!</h1>'
-    return render_template('signup.html', form=form)
-
-
-@app.route('/')
-def home():
-    return render_template("home.html")
-
-
-@app.route('/settings')
-@login_required
-def settings():
-    return render_template('settings.html', name=current_user.username)
+                login_user(user, remember=form.remember.data)            
+                update_this = User.query.filter_by(username=current_user.username).first()
+                update_this.logged_in = "True"
+                db.session.commit()
+                return redirect(url_for('home'))
+        return render_template('login.html', form=form, uname_help_block="block")
+    return render_template('login.html', form=form, uname_help_block="none", display_login="block", display_logout="none", display_signup="block")
 
 
 @app.route('/logout')
 @login_required
 def logout():
+    update_this = User.query.filter_by(username=current_user.username).first()
+    update_this.logged_in = "False"
+    db.session.commit()
     logout_user()
     return redirect(url_for('home'))
 
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    try:
+        if form.validate_on_submit():
+            hashed_password = generate_password_hash(form.password.data, method='sha256')
+            new_user = User(username=form.username.data, email=form.email.data, password=hashed_password, logged_in="True")
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('settings'))
+    except exc.SQLAlchemyError as e:
+        return render_template('signup.html', form=form, uname_help_block="block", display_login="block", display_logout="none", display_signup="block")
+
+    try:
+        if current_user.logged_in == "True":
+            return render_template('signup.html', form=form, display_login="none", display_logout="block", display_signup="none", query="Hello")
+    except:
+        return render_template('signup.html', form=form, display_login="block", display_logout="none", display_signup="block")
+
+
+@app.route('/')
+def home():
+    try:
+        if current_user.logged_in == "True":
+            return render_template("home.html", display_login="none", display_logout="block", display_signup="none", query="Hello")
+    except:
+        return render_template("home.html", display_login="block", display_logout="none", display_signup="block")
+
+
+@app.route('/settings')
+@login_required
+def settings():
+    try:
+        if current_user.logged_in == "True":
+            return render_template("settings.html", display_login="none", display_logout="block", display_signup="none", query="Hello")
+    except:
+        return render_template("settings.html", display_login="block", display_logout="none", display_signup="block")
+
+
+@app.route('/help')
+def help():
+    try:
+        if current_user.logged_in == "True":
+            return render_template("help.html", display_login="none", display_logout="block", display_signup="none", query="Hello")
+    except:
+        return render_template("help.html", display_login="block", display_logout="none", display_signup="block")
+
+
+@socketio.on('listen')
+def listen(msg):
+    send(msg, broadcast=True)
+
 if __name__ == '__main__': 
-    app.run(debug = True)
+    # app.run(debug = True)
+    socketio.run(app)
